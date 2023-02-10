0e64d007aad0eb45e7b997073f9f0233996fb633
allysonhoyos <124403258+allysonhoyos@users.noreply.github.com>
2023-02-10 23:58:07 +0100

    proyecto numero 2

---
+++ home.php
@@ -0,0 +1,133 @@
+<?php
+ /*Tipos de variables */
+ $string ="TEXT";
+ $int =1;
+ $suma=1+2;
+ echo $suma;
+ //condiciones;
+ $a=1;
+ $b=2;
+ if($a>$b) {
+  $resultado="es menor";
+ }
+ else{
+  $resultado="es mayor";
+ }
+ while($a <=10){
+  $a++;
+  if ($a=4){
+    $texto="gano";
+  }
+ }
+ $nota1=3
+ $nota2=4
+ $nota3=5
+ $nota4=2
+ $nota5=1
+ $nota6=3
+ $nota7=5
+ $nota8=2
+ $nota9=3
+ $nota10=4
+ $nota11=3
+ $nota12=2
+ $nota13=5
+ $nota14=4
+ $nota15=5
+  while($nota=1){
+    $nota="perdio"
+    if($nota=2){
+      $nota="perdio"
+    }
+    elseif ($nota=3){
+      $nota="paso raspado"
+    }
+    elseif($nota=4){
+      $nota="gano"
+    }
+    elseif($nota=5){
+      $nota="excelente"
+    }
+  } 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                  <a class="navbar-brand" href="#">Navbar</a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                      </li>
+                      <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                          Dropdown
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                          <li><a class="dropdown-item" href="#">Action</a></li>
+                          <li><a class="dropdown-item" href="#">Another action</a></li>
+                          <li><hr class="dropdown-divider"></li>
+                          <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                      </li>
+                    </ul>
+                    <form class="d-flex">
+                      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                      <button class="btn btn-outline-success" type="submit">Search</button>
+                    </form>
+                  </div>
+                </div>
+              </nav>
+        </div>
+    <div class="row">
+      <div class="col-3">
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+      <div class="col-6">
+        <div class="card" style="width: 18rem;">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-3">
+        <ul class="list-group">
+          <li class="list-group-item">An item</li>
+          <li class="list-group-item">A second item</li>
+          <li class="list-group-item">A third item</li>
+          <li class="list-group-item">A fourth item</li>
+          <li class="list-group-item">And a fifth one</li>
+        </ul>
+      </div>
+    </div>
+    </div>
+</body>
+</html>
